1479aa4d581a6e3eed500161e51c9cdbd300bf82
junjiewu <454412633@qq.com>
2020-07-02 16:14:47 -0700

    creact

---
+++ mapReduce/mr/rpc.go
@@ -0,0 +1,54 @@
+package mr
+
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+import "os"
+import "strconv"
+
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
+
+type Args struct {
+	FileName string
+    JobNumber int
+    JobType int
+    Inprogress bool
+    Finished bool
+    JN int
+}
+
+type Reply struct {
+    FileName string
+    JobNumber int
+    JobType int
+    Inprogress bool
+    Finished bool
+    JN int
+}
+
+// Add your RPC definitions here.
+type Job struct {
+    FileName string
+    JobNumber int
+    JobType int
+    Inprogress bool
+    Finished bool
+    JN int
+}
+
+
+// Cook up a unique-ish UNIX-domain socket name
+// in /var/tmp, for the master.
+// Can't use the current directory since
+// Athena AFS doesn't support UNIX-domain sockets.
+func masterSock() string {
+	s := "/var/tmp/824-mr-"
+	s += strconv.Itoa(os.Getuid())
+	return s
+}
